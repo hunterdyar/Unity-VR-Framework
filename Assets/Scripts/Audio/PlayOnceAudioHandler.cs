4ebcdca8a56cda092d526bdacb69ce72d0939775
Hunter Dyar <bloop@hdyar.com>
2020-01-31 16:27:00 -0500

    Added playing an audio on interacting with an interactable.
    The demo is set up for "Activation" which is pressing the trigger after an object is held, like firing a gun. In many cases, this will want to be changed to the "selection Enter" interaction event, where its just when you pick up to investigate an object, where something happens.

---
+++ Assets/Scripts/Audio/PlayOnceAudioHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayOnceAudioHandler : MonoBehaviour
+{
+    [Tooltip("Optional. Define the clip here or just in the audio source component.")]
+    public AudioClip sourceClip;
+    private AudioSource audioSource;
+
+    private bool played = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+        #region Tests
+        if(audioSource.playOnAwake)
+        {
+            Debug.LogWarning("Audio source for play once handler is set to play on awake. This is probably unintentional.");
+        }
+        #endregion
+        
+        if(sourceClip != null)
+        {
+            audioSource.clip = sourceClip;
+        }
+    }
+    public void Play()
+    {
+        if(played == false)
+        {
+            audioSource.Play();
+            played = true;
+        }
+    }
+    public void Reset()
+    {
+        played = false;
+    }
+}
