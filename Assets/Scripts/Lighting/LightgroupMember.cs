f05fd430f877ea6ae7a0535a2e4dc28480cf4ec2
Hunter Dyar <bloop@hdyar.com>
2020-02-05 13:45:17 -0500

    Begun Lighting

---
+++ Assets/Scripts/Lighting/LightgroupMember.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IMMToolkit{
+    [RequireComponent(typeof(Light))]
+    public class LightgroupMember : MonoBehaviour
+    {
+        [HideInInspector]
+        public new Light light;
+        public LightingGroup lightingGroup;
+        
+        void Awake(){
+            light = GetComponent<Light>();
+        }
+        void OnEnable(){
+            lightingGroup.RegisterMember(light);
+        }
+        void OnDisable(){
+            lightingGroup.RegisterMember(light);
+        }
+    }
+}
---
+++ Assets/Scripts/Lighting/SO/LightingGroup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace IMMToolkit{
+[CreateAssetMenu(fileName = "LightGroup", menuName = "IMMToolkit/LightingGroup", order = 1)]
+public class LightingGroup : ScriptableObject
+{   
+    private List<Light> members;
+    public void RegisterMember(Light source){
+        if(members == null)
+        {
+            members = new List<Light>();
+        }
+        if(!members.Contains(source))
+        {
+            members.Add(source);
+        }
+        else
+        {
+            Debug.LogWarning("Audio Source already member of group: "+this.name,source);
+        }
+    }
+    public void DeregisterMember(Light source){
+        if(members == null)
+        {
+            return;
+        }
+        if(members.Contains(source))
+        {
+            members.Remove(source);
+        }
+    }
+}
+}
