0341b4de308cde1fd7201caf97c74451bcb67074
Hunter Dyar <bloop@hdyar.com>
2020-02-07 13:18:13 -0500

    Ambient lightGroupMember now extends lightGroupMember. Add it to an empty object and it will adjust ambient light intensity as if it was a member of a lightGroup.
    Example scenes updated for demos, lightGroupMember updated to support the overrides.

---
+++ Assets/IMMToolkit/Scripts/Lighting/AmbientLightgroupmember.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+//Allows the ambient scene lighting to be a member of a lighting group, and fade with them.
+
+namespace IMMToolkit {
+    public class AmbientLightgroupmember : LightgroupMember
+    {
+        void Awake(){
+            defaultIntensity = RenderSettings.ambientIntensity;
+        }
+        //RenderSettings.AmbientLight
+        public override void FadeOut(float timeToFade)
+        {
+            
+            if(RenderSettings.ambientIntensity != 0)
+            {
+                StartCoroutine(FadeLight(RenderSettings.ambientIntensity,0,timeToFade));
+            }
+        }
+        public override void FadeIn(float timeToFade)
+        {
+            Debug.Log("fade out ambient");
+            if(RenderSettings.ambientIntensity != defaultIntensity)
+            {
+                StartCoroutine(FadeLight(RenderSettings.ambientIntensity,defaultIntensity,timeToFade));
+            }
+        }
+        public override IEnumerator FadeLight(float startIntensity, float endIntensity,float timeToFade)
+        {
+            float t = 0;
+            while(t<1)
+            {
+                t = t + Time.deltaTime/timeToFade;
+                RenderSettings.ambientIntensity = Mathf.Lerp(startIntensity,endIntensity,t);
+                yield return new WaitForEndOfFrame();
+            }
+            RenderSettings.ambientIntensity = endIntensity;
+        }
+    }
+
+}
--- Assets/IMMToolkit/Scripts/Lighting/LightgroupMember.cs
+++ Assets/IMMToolkit/Scripts/Lighting/LightgroupMember.cs
@@ -3,39 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace IMMToolkit{
-    [RequireComponent(typeof(Light))]
     public class LightgroupMember : MonoBehaviour
     {
-        public LightingGroup lightingGroup;
+        public LightingGroup[] lightingGroups;
 
         [HideInInspector]
         public new Light light;
-        private float defaultIntensity;//the "on"
+        [HideInInspector]
+        public float defaultIntensity;//the "on"
         void Awake(){
             light = GetComponent<Light>();
+            if(light == null){
+                Debug.LogError("no light component found for lightGroupMember",gameObject);
+            }
             defaultIntensity = light.intensity;
         }
         void OnEnable(){
-            lightingGroup.RegisterMember(this);
+            foreach(LightingGroup lightingGroup in lightingGroups)
+            {
+                lightingGroup.RegisterMember(this);
+            }
         }
         void OnDisable(){
-            lightingGroup.DeregisterMember(this);
+            foreach(LightingGroup lightingGroup in lightingGroups)
+            {
+                lightingGroup.DeregisterMember(this);
+            }
         }
-        public void FadeOut(float timeToFade)
+        public virtual void FadeOut(float timeToFade)
         {
             if(light.intensity != 0)//prevent uneccesary coroutines
             {
                 StartCoroutine(FadeLight(light.intensity,0,timeToFade));
             }
         }
-        public void FadeIn(float timeToFade)
+        public virtual void FadeIn(float timeToFade)
         {
             if(light.intensity != defaultIntensity)//nothing would happen anyway
             {
                 StartCoroutine(FadeLight(light.intensity,defaultIntensity,timeToFade));
             }
         }
-        public IEnumerator FadeLight(float startIntensity, float endIntensity,float timeToFade)
+        public virtual IEnumerator FadeLight(float startIntensity, float endIntensity,float timeToFade)
         {
             float t = 0;
             while(t<1)
